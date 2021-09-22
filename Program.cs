91e31be6cc1ae5c5aa89e92482d5def5b41a808d
mauricio <mauriciocon@gmail.com>
2021-09-21 21:12:13 -0300

    Starting with FindFiles

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
 namespace files_module
 {
@@ -6,7 +8,29 @@ namespace files_module
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var files = FindFiles("stores");
+
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        static IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+    
+            var foundFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+    
+            foreach (var file in foundFiles)
+            {
+                if (file.EndsWith("sales.json"))
+                {
+                    salesFiles.Add(file);
+                }
+            }
+    
+            return salesFiles;
         }
     }
 }
