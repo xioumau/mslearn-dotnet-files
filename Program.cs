40029e0df3142c034039ee9123f8a692cd092ebc
Mauricio de Souza <55670845+xioumau@users.noreply.github.com>
2021-09-24 10:42:28 -0300

    Cria método para calcular total de vendas

--- Program.cs
+++ Program.cs
@@ -38,5 +38,30 @@ namespace files_module
     
             return salesFiles;
         }
+
+        class SalesData
+        {
+            public double Total { get; set; }
+        }
+
+        public double CalculateSalesTotal(IEnumerable<String> salesFiles)
+        {
+            double salesTotal = 0;
+
+                // itera sobre cada caminho em salesFiles
+                foreach(var file in salesFiles)
+                {
+                    // lê o conteúdo do arquivo
+                    String salesJson = File.ReadAllText(file);
+
+                    // converte para Json
+                    SalesData data = JsonConvert.DeserializeObject<SalesData>(salesJson);
+
+                    // adiciona o montante encontrado no campo Total e adicio à variável salesTotal 
+                    salesTotal += data.Total;
+                }
+
+            return salesTotal
+        }
     }
 }
